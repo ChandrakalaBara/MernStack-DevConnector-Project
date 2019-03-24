7da3258a9bb6c9e167ef562f50168f1edaa26bd3
Chandrakala <chandrakala.bara@gmail.com>
2019-03-24 19:18:35 +0530

    Creating jwt and passport jwt authentication

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-    mongoURI: 'mongodb://chandrakala:mernstack3@ds349455.mlab.com:49455/mernstack-dev-con'
+    mongoURI: 'mongodb://chandrakala:mernstack3@ds349455.mlab.com:49455/mernstack-dev-con',
+    secretOrKey: 'secret'
 }
